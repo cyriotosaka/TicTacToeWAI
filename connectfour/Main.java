d2c4b80a93ee201bf3a89044a35449dbbb13fa3c
okky priscila <108847596+cyriotosaka@users.noreply.github.com>
2024-12-10 12:50:03 +0700

    Initial commit

---
+++ connectfour/Main.java
@@ -0,0 +1,9 @@
+package connectfour;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Connect Four Game");
+        ConnectFour game = new ConnectFour();
+        game.play();
+    }
+}
